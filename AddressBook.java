bfd4b8b046224dff0759f062b637ac4295051d82
tejasdcr7 <tejasdandge@gmail.com>
2021-12-22 17:47:42 +0530

    uc2 added

---
+++ AddressBook.java
@@ -0,0 +1,68 @@
+package com.bridgelabz.addbook;
+import java.util.Scanner;
+
+
+public class AddressBook {
+
+    public static void main(String[] args) {
+        AddressBook a = new AddressBook();
+        a.showMenu();
+        a.userSelection();
+        a.editContact();
+
+    }
+
+    public int showMenu() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" Welcome to the Address Book \n 1.Add Contact\n 2.Edit Contact\n 3.Delete Contact\n");
+
+        int option = sc.nextInt();
+        return option;
+
+    }
+
+    void userSelection(int option) {
+        switch (option) {
+
+            case 1:
+                addContact();
+                break;
+            case 2:
+
+                break;
+            case 3:
+                break;
+        }
+
+    }
+
+    void addContact() {
+        Scanner sc = new Scanner(System.in);
+        ContactDetails contact = new ContactDetails();
+        System.out.println("Enter First Name");
+        contact.First_Name = sc.next();
+        System.out.println("Enter Last Name");
+        contact.Last_Name = sc.next();
+        System.out.println("Enter Address");
+        contact.Address = sc.next();
+        System.out.println("Enter City Name");
+        contact.City = sc.next();
+        System.out.println("Enter State");
+        contact.State = sc.next();
+        System.out.println("Enter Phone Number ");
+        contact.Phone_Number = sc.next();
+        System.out.println("Enter Email Id");
+        contact.Email = sc.next();
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# Addressbook-day9
